a6c9c56b92ab78199558e7f8d4dbaffd88873728
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-08-18 12:47:09 +0430

    Added media.css

--- public/index.php
+++ public/index.php
@@ -2,6 +2,13 @@
     require "src/includes/head.php";
 ?>
 <body>
-<?php
-    require "src/includes/header.php";
-?>
+    <?php
+        require "src/includes/header.php";
+    ?>
+    <?php
+        
+    ?>
+    <?php
+        require "src/includes/footer.php";
+    ?>
+</body>
