3584e127dfdfeade1163e4455f226e68cf16b632
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-09-01 10:36:49 -0400

    Details changed

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
+    require "src/includes/db.php";
+?>
+<?php
     require "src/includes/head.php";
 ?>
 <body>
     <?php
         require "src/includes/header.php";
-    ?>
-    <?php
-        require "src/includes/middle.php";
     ?>
     <?php
         require "src/includes/footer.php";
