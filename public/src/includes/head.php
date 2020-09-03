7fd435bce392c40d54cfb181d35effda49f7c560
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-09-03 12:24:00 +0430

    Details are changed

--- public/src/includes/head.php
+++ public/src/includes/head.php
@@ -6,9 +6,12 @@
     <title>CMS</title>
     <meta name="keywords" content="" />
     <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <meta name="robots" content="index.follow" />
     <!-- tailwind css -->
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />
-    <link rel="stylesheet" type="text/css" href="src/css/index.css"/>
+	<!-- own css -->
+    <link rel="stylesheet" type="text/css" href="src/css/index.css" />
+    <link rel="stylesheet" type="text/css" href="src/css/fonts.css" />
+    <link rel="stylesheet" type="text/css" href="src/css/media.css" />
 </head>
