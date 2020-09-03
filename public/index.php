0a3648a227c724c9df22e9ee267bd23c4f407428
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-09-03 08:22:53 +0430

    Details changed

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
         require "src/includes/header.php";
     ?>
     <div class="middle">
-        
+       <p>website content</p> 
     </div>
     <?php
         require "src/includes/footer.php";
