b4917e56f7870895a7c8e936e23d88c75beaec32
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-09-19 13:12:07 -0400

    SMALL CHANGES

--- public/src/includes/header.php
+++ public/src/includes/header.php
@@ -1,7 +1,17 @@
 <header class="header">
     <div class="menu">
         <?php
-            
+            $hostName = 'localhost';
+            $userName = 'root';
+            $userPassword = '';
+            $databaseName = 'menu';
+            $mysqli = new MySQLi($hostName , $userName , $userPassword , $databaseName);
+            $sql = "SELECT * FROM lists";
+            $result = $mysqli->query($sql); 
+            while($row = $result->fetch_assoc()){
+                $list_title = $row['list_title'];
+                echo $list_title;
+            }
         ?>
     </div>
 </header>
