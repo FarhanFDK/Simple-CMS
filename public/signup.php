e256c380e55c8c25296ec4b5003f05481f140d67
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-10-09 16:51:11 +0330

    CHANGED

--- public/signup.php
+++ public/signup.php
@@ -6,28 +6,31 @@
 		<form class="text-center">
 			<div>
 				<label class="mr-2 text-gray-700 text-l font-bold mb-2 text-none" for="firstname">نام: </label><span class="text-red-700">*</span>
-				<input class="firstname my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="firstname" name="firstname" autocomplete="FALSE"/>
-				<span class="text-gray-700 text-l font-bold mb-2 text-none" id="name"></span>
+				<input class="firstname my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="firstname" name="firstname" maxlength="20" autocomplete="FALSE"/>
+				<div><span class="hidden text-red-700 text-l font-bold mb-2 text-none" id="firstname_blur">نام باید بین 3 تا 20 کاراکتر باشد</span></div>
 			</div>
 			<div>
 				<label class="text-gray-700 text-l font-bold mb-2 text-none" for="firstname">نام خانوادگی: </label><span class="text-red-700">*</span>
-				<input class="ml-12 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="firstname" name="firstname" autocomplete="FALSE"/>
+				<input class="ml-12 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="lastname" name="lastname" maxlength="20" autocomplete="FALSE"/>
+				<div><span class="hidden text-red-700 text-l font-bold mb-2 text-none" id="lastname_blur">نام خانوادگی باید بین 4 تا 20 کاراکتر باشد</span></div>
 			</div>
 			<div>
 				<label class="text-gray-700 text-l font-bold mb-2 text-none" for="phonenumber">شماره همراه: </label><span class="text-red-700">*</span>
-				<input class="ml-12 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="phonenumber" name="phonenumber" autocomplete="FALSE"/>
+				<input class="phonenumber ml-12 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="phonenumber" name="phonenumber" maxlength="15" autocomplete="FALSE"/>
+				<div><span class="hidden text-red-700 text-l font-bold mb-2 text-none" id="phonenumber_blur">شماره همراه باید حداقل 11 رقم باشد</span></div>
 			</div>
 			<div>
 				<label class="text-gray-700 text-l font-bold mb-2 text-none" for="email">ایمیل: </label>
-				<input class="my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" id="email" name="email" autocomplete="FALSE"/>
+				<input class="my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" id="email" name="email" maxlength="30" autocomplete="FALSE"/>
 			</div>
 			<div>
 				<label class="text-gray-700 text-l font-bold mb-2" for="password">رمز عبور: </label><span class="text-red-700">*</span>
-				<input class="ml-4 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" id="password" name="password" autocomplete="FALSE"/>
+				<input class="ml-4 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" id="password" name="password" maxlength="32" autocomplete="FALSE"/>
 			</div>
 			<div>
 				<label class="text-gray-700 text-l font-bold mb-2" for="password_confirm">تایید رمز عبور: </label><span class="text-red-700">*</span>
-				<input class="ml-12 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" id="password_confirm" name="password_confirm" autocomplete="FALSE"/>
+				<input class="ml-12 my-4 shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" id="password_confirm" name="password_confirm" maxlength="32" autocomplete="FALSE"/>
+				<div><span class="hidden text-red-700 text-l font-bold mb-2 text-none" id="password_blur">رمزعبور همخوانی ندارد</span></div>
 			</div>
 			<div>
 				<div>		
@@ -35,7 +38,7 @@
 					<label class="text-gray-700 text-l font-bold mb-2" for="privacy_policy"><a class="text-blue-400" href="policy.php" title="" target="_blank">شرایط</a> را خوانده و قبول دارم</label><span class="text-red-700">*</span>
 				</div>
 				<div>
-				<input class="ml-2 cursor-pointer" type="checkbox" id="requests" name="requests" />
+					<input class="ml-2 cursor-pointer" type="checkbox" id="requests" name="requests" />
 					<label class="text-gray-700 text-l font-bold mb-2" for="requests">مایل به دریافت ایمیل هستم</label>
 				</div>
 			</div>
@@ -47,19 +50,111 @@
 </div>
 <script>
 	$(document).ready(function (){
-		$(document).keypress(function(e){
+		/*$(document).keypress(function(e){
 			let key = e.which;
 			if(e.which == 13){
-				$("#submit").focus();
-			} 
-		});
+				$("#submit").click();
+			}
+		});*/
+
 		$("#firstname").keypress(function(e){
 			let key = e.which;
 			// alert(key);
 			if(key < 58 && key > 47 || key == 45  || key == 61 || key == 42 || key == 47 || key == 43){
 				e.preventDefault();
 			}
-		})
-	});
+			let valueF = $("#firstname").val();
+			if(valueF.length >= 2){
+				$("#firstname_blur").fadeOut();
+				$("#submit").prop('disabled', false);
+			}
+		});
+
+		$("#firstname").blur(function(){
+			let value = $("#firstname").val();
+			if(value.length < 3){
+				$("#firstname_blur").fadeIn();
+				$("#submit").prop('disabled', true);
+			}else{
+				$("#firstname_blur").fadeOut();
+				$("#submit").prop('disabled', false);
+			}
+		});
+
+		$("#lastname").keypress(function(e){
+			let key = e.which;
+			// alert(key);
+			if(key < 58 && key > 47 || key == 45  || key == 61 || key == 42 || key == 47 || key == 43){
+				e.preventDefault();
+			}
+			let valueL = $("#lastname").val();
+			if(valueL.length >= 3){
+				$("#lastname_blur").fadeOut();
+				$("#submit").prop('disabled', false);
+			}
+		});
+
+		$("#lastname").blur(function(){
+			let value = $("#lastname").val();
+			if(value.length < 4){
+				$("#lastname_blur").fadeIn();
+				$("#submit").prop('disabled', true);
+			}else{
+				$("#lastname_blur").fadeOut();
+				$("#submit").prop('disabled', false);
+			}
+		});
+		$("#phonenumber").blur(function(){
+			let value = $("#phonenumber").val();
+			if(value.length < 11){
+				$("#phonenumber_blur").fadeIn();
+				$("#submit").prop('disabled', true);
+			}else{
+				$("#phonenumber_blur").fadeOut();
+				$("#submit").prop('disabled', false);
+			}
+		});
+		$("#phonenumber").keypress(function(e){
+			let key = e.which;
+			// alert(key);
+			if(!(key < 58 && key > 47)){
+				e.preventDefault();
+			}
+			let valueN = $("#phonenumber").val();
+			if(valueN.length >= 10){
+				$("#phonenumber_blur").fadeOut();
+				$("#submit").prop('disabled', false);
+			}
+		});
+		
+		$("#submit2").on('click' , function(){
+        var firstname = $("#firstname").val();
+		var lastname = $("#lastname").val();
+		var email = $("#email").val();
+		var phonenumber = $("phonenumber").val();
+        if(firstname == "" || lastname == "" || phonenumber == "" || password == "" || password_confirm == ""){
+            alert("Please complete the form");
+        }else{
+            $.ajax({
+                url:'http://localhost/ajax/public/src/includes/update.php',
+                method:'POST',
+                data:{
+                    submit: 1,
+                    firstnamePHP:firstname,
+					lastnamePHP:lastname,
+					emailPHP:email,
+					phonenumberPHP:phonenumber,
+					passwordPHP:password
+                },
+                success:function(response){
+                    $("#response").html(response);
+                    // $("#submit").attr("disabled" , "true");
+                    console.log("SUCCESS!");
+                },
+                dataType: 'text'
+            });
+        }
+    });
+});
 </script>
 <?php require "src/includes/footer.php"; ?>
