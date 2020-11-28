7decfddee338ec731bfbe4f50c8beacdbca6c0c7
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-11-28 08:04:53 +0330

    CHANGED

--- public/src/includes/head.php
+++ public/src/includes/head.php
@@ -21,8 +21,7 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <link rel="stylesheet" href="src/css/index.css" />
         <link rel="stylesheet" href="src/css/media.css" />
-        <!--<link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />-->
-        <link rel="stylesheet" href="src/css/tailwind.css"/>
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />
         <link rel="shortcut icon" href="src/images/icons/.." />
         <link rel="icon" href="src/images/icons/.." />
         <script src="https://kit.fontawesome.com/b39b75221a.js" crossorigin="anonymous"></script>
